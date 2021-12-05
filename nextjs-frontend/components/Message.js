3d58e78d7d8fcb62bae48e321e37903ad4292bb3
d3vv3 <jaimecondel@gmail.com>
2021-12-05 21:38:56 +0100

    feat: upd message

--- nextjs-frontend/components/Message.js
+++ nextjs-frontend/components/Message.js
@@ -7,8 +7,14 @@ function Message(props) {
   return (
     <div className="message-container">
       <div className="message">
-        El próximo pedido será el 12 de diciembre. Realiza tu pedido antes del
-        12 para recibirlo cuanto antes.
+        <p>
+          El próximo pedido será el 12 de diciembre. Realiza tu pedido antes del
+          12 para recibirlo cuanto antes.
+        </p>
+        <p>
+          Haz click en el modelo para ver todos los colores disponibles. ¡Hay
+          más de 25!
+        </p>
       </div>
     </div>
   );
